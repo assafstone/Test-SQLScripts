aa587e2c3566afd77ab9a249419dfe33546a1d48
Assaf Stone <stoneas@gmail.com>
2019-12-10 13:20:22 -0500

    Updated Test-SqlScripts.ps1 to function as a Pester test

--- Test-SQLScripts.ps1
+++ Test-SQLScripts.ps1
@@ -1,18 +1,49 @@
-[cmdletbinding()]
-param(
-    [Parameter(
-        Mandatory = $true,
-        Position = 0,
-        ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true)
-    ] [System.IO.FileInfo] $Files,
-    [Parameter(Mandatory = $false)] [string] $PathToScriptDomLibrary = $null,
-    [Parameter(Mandatory = $false)] [string] $UseQuotedIdentifier = $true
-)
+Describe "Verify SQL scripts do not allow for breaking changes" {
+    Import-Module .\Modules\SqlScriptParser\Get-SqlServerDomParserKeys.psm1
+    Import-Module .\Modules\SqlScriptParser\Invoke-SqlScriptParser.psm1
+    Install-Module -Name Assert
+    Import-Module Assert
+    
+    if (!$Env:Files) {
+        $Env:Files = $null
+    }
+    if (!$Env:PathToScriptDomLibrary) {
+        $Env:PathToScriptDomLibrary = $null
+    }
+    if (!$Env:UseQuotedIdentifier) {
+        $Env:UseQuotedIdentifier = $null
+    }
+    Write-Host "Files: $env:Files"
 
-Import-Module .\Modules\SqlScriptParser\Get-SqlServerDomParserKeys.psm1
-Import-Module .\Modules\SqlScriptParser\Invoke-SqlScriptParser.psm1
+    $results = (Get-ChildItem $Env:Files) | Invoke-SqlScriptParser `
+        -PathToScriptDomLibrary $Env:PathToScriptDomLibrary `
+        -UseQuotedIdentifier $Env:UseQuotedIdentifier
 
-$Files | Invoke-SqlScriptParser `
-    -PathToScriptDomLibrary $PathToScriptDomLibrary `
-    -UseQuotedIdentifier $UseQuotedIdentifier
+    $statements = $results.Batches.Statements
+
+    It "The script does not alter the column of any tables" {
+        $found = $statements | Where-Object StatementType -Like "AlterTableAlterColumnStatement"
+
+        if ($found) {
+            $customMessage = "Found $($found.Count) instances of $($found[0].StatementType):"
+            foreach ($item in $found) {
+                $customMessage = $customMessage + "`r`n`ton line #$($item.LineNumber): $($item.Text)"
+            }
+        }
+        # $found | Should -BeNullOrEmpty -because "Line #$($found.LineNumber): $($found.Text)"
+        $found.Count | Assert-Equal -Expected 0 -CustomMessage $customMessage
+    }
+
+    It "The script does not drop elements of a table" {
+        $found = $statements | Where-Object StatementType -Like "AlterTableDropTableElementStatement"
+
+        if ($found) {
+            $customMessage = "Found $($found.Count) instances of $($found[0].StatementType):"
+            foreach ($item in $found) {
+                $customMessage = $customMessage + "`r`n`ton line #$($item.LineNumber): $($item.Text)"
+            }
+        }
+        # $found | Should -BeNullOrEmpty -because "Line #$($found.LineNumber): $($found.Text)"
+        $found.Count | Assert-Equal -Expected 0 -CustomMessage $customMessage
+    }
+}
