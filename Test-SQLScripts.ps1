de4ec05c5c47a612d66c9b0a3fecb272143493db
Assaf Stone <asstone@microsoft.com>
2020-07-20 08:52:51 -0400

    updated to use new Pester syntax

--- Test-SQLScripts.ps1
+++ Test-SQLScripts.ps1
@@ -1,35 +1,20 @@
-# Add items to the $verifcationTests hashtable, in order to add tests. 
+# Add items to the $verificationTests hashtable, in order to add tests. 
 # The hash's key is the Microsoft.SqlServer.TransactSql.ScriptDom.TSqlStatement descendant class we are checking, and the value is the Pester test assumption
 # If you add a statement-type and get a Parser Key missing warning, add it to the Get-SqlServerDomParserKeys function.
-$verifcationTests = @{
-    AlterTableAlterColumnStatement      = "The script does not alter the column of any tables";
-    AlterTableDropTableElementStatement = "The script does not drop any table elements";
-    DropTableStatement                  = "The script does not drop any tables";
-}
-
+$violations = 
+    @{violation = 'AlterTableAlterColumnStatement'}, 
+    @{violation = 'AlterTableDropTableElementStatement'}, 
+    @{violation = 'DropTableStatement'}
+    
 Describe "When migrating SQL schema changes to the database" {
+    
     Import-Module $PSScriptRoot/Modules/SqlScriptParser/Get-SqlServerDomParserKeys.psm1
     Import-Module $PSScriptRoot/Modules/SqlScriptParser/Invoke-SqlScriptParser.psm1
-        
+    
     Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
     Install-Module -Name Assert -Scope CurrentUser -SkipPublisherCheck
     Import-Module Assert
-    
-    function Test-ViolatingStatements {
-        param (
-            [Parameter(Mandatory = $true)][string] $StatementType
-        )
-        $found = $statements | Where-Object StatementType -Like $StatementType
-
-        if ($found) {
-            $customMessage = "Found $($found.Count) instances of $($found[0].StatementType):"
-            foreach ($item in $found) {
-                $customMessage = $customMessage + "`r`n`ton line #$($item.LineNumber): $($item.Text)"
-            }
-        }
-        $found.Count | Assert-Equal -Expected 0 -CustomMessage $customMessage
-    }
-
+      
     if (!$Env:PathToSql) {
         $Env:PathToSql = $null
     }
@@ -39,17 +24,25 @@ Describe "When migrating SQL schema changes to the database" {
     if (!$Env:UseQuotedIdentifier) {
         $Env:UseQuotedIdentifier = $null
     }
-    Write-Host "Files: $Env:PathToSql"
 
+    Write-Debug "Files: $Env:PathToSql"
+    
     $results = (Get-ChildItem $Env:PathToSql) | Invoke-SqlScriptParser `
-        -PathToScriptDomLibrary $Env:PathToScriptDomLibrary `
-        -UseQuotedIdentifier $Env:UseQuotedIdentifier
-
+    -PathToScriptDomLibrary $Env:PathToScriptDomLibrary `
+    -UseQuotedIdentifier $Env:UseQuotedIdentifier
+    
     $statements = $results.Batches.Statements
-
-    $verifcationTests.Keys | ForEach-Object {
-        It $verifcationTests.Item($_) {
-            Test-ViolatingStatements -StatementType $_
+   
+    It "The script shouldn't have a <violation> statement" -TestCases $violations {
+        param ($violation)
+        $found = $statements | Where-Object StatementType -Like $violation
+    
+        if ($found) {
+            $customMessage = "Found $($found.Count) instances of $($found[0].StatementType):"
+            foreach ($item in $found) {
+                $customMessage = $customMessage + "`r`n`ton line #$($item.LineNumber): $($item.Text)"
+            }
         }
+        $found.Count | Assert-Equal -Expected 0 -CustomMessage $customMessage
     }
 }
